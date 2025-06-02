dc5f893bc243f6097eee1573bdbd16688ba9af0b
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-06-02 12:24:50 +0600

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import cluster from 'cluster';
 import express, { NextFunction, Request, Response } from "express";
 import os from 'os';
 import path from "path";
@@ -14,80 +13,80 @@ import { logger } from "./utils/logger";
 
 const numCPUs = os.cpus().length || 1;
 
-if (cluster.isPrimary) {
-  console.log(`primary process ${process.pid} running`)
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} exited. Restarting...`);
-    cluster.fork();
-  });
-} else {
-  app.post(
-    "/payment/complete",
-    express.raw({ type: "application/json" }),
-    asyncWrapper(payment_controller.webhook),
+// if (cluster.isPrimary) {
+//   console.log(`primary process ${process.pid} running`)
+//   for (let i = 0; i < numCPUs; i++) {
+//     cluster.fork();
+//   }
+//   cluster.on('exit', (worker, code, signal) => {
+//     console.log(`Worker ${worker.process.pid} exited. Restarting...`);
+//     cluster.fork();
+//   });
+// } else {
+app.post(
+  "/payment/complete",
+  express.raw({ type: "application/json" }),
+  asyncWrapper(payment_controller.webhook),
+);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req: Request, res: Response) => {
+  res.render("default");
+});
+
+middleware(app);
+// route middleware
+routeMiddleware(app);
+
+app.use(globalErrorHandler);
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const error = new CustomError(
+    `Can't find ${req.originalUrl} on the server`,
+    404,
   );
+  globalErrorHandler(error, req, res, next);
+});
+function main() {
+  try {
+    server.listen(Number(config?.PORT), async () => {
+      logger.info("server is running");
+      await connectToDB();
+      logger.info(
+        `Worker process ${process.pid} is running on port ${config.PORT}`,
+      );
 
-  app.set("views", path.join(__dirname, "views"));
-  app.set("view engine", "ejs");
-
-  app.get("/", (req: Request, res: Response) => {
-    res.render("default");
-  });
-
-  middleware(app);
-  // route middleware
-  routeMiddleware(app);
-
-  app.use(globalErrorHandler);
-
-  app.all("*", (req: Request, res: Response, next: NextFunction) => {
-    const error = new CustomError(
-      `Can't find ${req.originalUrl} on the server`,
-      404,
-    );
-    globalErrorHandler(error, req, res, next);
-  });
-  function main() {
-    try {
-      server.listen(Number(config?.PORT), async () => {
-        logger.info("server is running");
-        await connectToDB();
-        logger.info(
-          `Worker process ${process.pid} is running on port ${config.PORT}`,
-        );
-
-        // Error handling for worker processes
-        process.on("uncaughtException", (error) => {
-          console.error("Uncaught Exception:", error.message);
-          console.error(error.stack);
-        });
-
-        process.on("unhandledRejection", (reason, promise) => {
-          console.error("Unhandled Rejection at:", promise, "reason:", reason);
-        });
-
-        process.on("SIGTERM", () => {
-          console.log(
-            `Worker process ${process.pid} received SIGTERM. Shutting down gracefully.`,
-          );
-          process.exit(0);
-        });
-
-        process.on("SIGINT", () => {
-          console.log(
-            `Worker process ${process.pid} received SIGINT. Shutting down gracefully.`,
-          );
-          //   process.exit(0);
-        });
+      // Error handling for worker processes
+      process.on("uncaughtException", (error) => {
+        console.error("Uncaught Exception:", error.message);
+        console.error(error.stack);
       });
-    } catch (error) {
-      console.log(error);
-      // logger.error(error)
-    }
-  }
 
-  main();
+      process.on("unhandledRejection", (reason, promise) => {
+        console.error("Unhandled Rejection at:", promise, "reason:", reason);
+      });
+
+      process.on("SIGTERM", () => {
+        console.log(
+          `Worker process ${process.pid} received SIGTERM. Shutting down gracefully.`,
+        );
+        process.exit(0);
+      });
+
+      process.on("SIGINT", () => {
+        console.log(
+          `Worker process ${process.pid} received SIGINT. Shutting down gracefully.`,
+        );
+        //   process.exit(0);
+      });
+    });
+  } catch (error) {
+    console.log(error);
+    // logger.error(error)
+  }
 }
+
+main();
+// }
